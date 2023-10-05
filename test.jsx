cd300c969a64e4007cbfbc9f26d187b68d37d600
Faxraddin <reydivonay@gmail.com>
2023-10-05 20:14:47 +0400

    d

--- test.jsx
+++ test.jsx
@@ -5,24 +5,25 @@ function findLongestWordWithMostVowels(sentence) {
       .filter(word => word.length > 0);
   
     if (!words.length) {
-      return null; // No valid words found
+      return null; 
     }
   
-    // Find the length of the longest word
+    
     const maxLength = Math.max(...words.map(word => word.length));
   
-    // Filter words that have the longest length
+    
     const longestWords = words.filter(word => word.length === maxLength);
   
-    // Function to count vowels in a word
+    
     function countVowels(word) {
       const vowels = "aeiou";
       return word.split("").filter(char => vowels.includes(char)).length;
     }
   
-    // Find the word with the highest number of vowels among the longest words
+   
     let maxVowels = 0;
     let longestWordWithMostVowels = longestWords[0];
+    
     for (const word of longestWords) {
       const vowelCount = countVowels(word);
       if (vowelCount > maxVowels) {
